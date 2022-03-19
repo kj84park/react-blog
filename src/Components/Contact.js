63e6e0604fc7ea1139b2c8b8af478b4982d28dac
kyungjoon.park <kyungjoon.park@linecorp.com>
2022-03-20 01:08:56 +0900

    Initial commit (10)

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -12,6 +12,8 @@ import {
     TwitterVideoEmbed,
     TwitterOnAirButton
 } from 'react-twitter-embed';
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
 
 class Contact extends Component {
     render() {
@@ -29,89 +31,37 @@ class Contact extends Component {
             <section id="contact">
                 <Fade bottom duration={1000}>
                     <div className="row section-head">
-                        <div className="two columns header-col">
-                            <h1>
-                                <span>Get In Touch.</span>
-                            </h1>
-                        </div>
+                        {/*<div className="two columns header-col">*/}
+                        {/*    <h1>*/}
+                        {/*        <span>Get In Touch.</span>*/}
+                        {/*    </h1>*/}
+                        {/*</div>*/}
 
-                        <div className="ten columns">
-                            <p className="lead">{message}</p>
-                        </div>
+                        {/*<div className="ten columns">*/}
+                        {/*    <p className="lead">{message}</p>*/}
+                        {/*</div>*/}
                     </div>
                 </Fade>
 
                 <div className="row">
                     <Slide left duration={1000}>
                         <div className="eight columns">
-                            <form action="" method="post" id="contactForm" name="contactForm">
-                                <fieldset>
-                                    <div>
-                                        <label htmlFor="contactName">
-                                            Name <span className="required">*</span>
-                                        </label>
-                                        <input
-                                            type="text"
-                                            defaultValue=""
-                                            size="35"
-                                            id="contactName"
-                                            name="contactName"
-                                            onChange={this.handleChange}
-                                        />
-                                    </div>
-
-                                    <div>
-                                        <label htmlFor="contactEmail">
-                                            Email <span className="required">*</span>
-                                        </label>
-                                        <input
-                                            type="text"
-                                            defaultValue=""
-                                            size="35"
-                                            id="contactEmail"
-                                            name="contactEmail"
-                                            onChange={this.handleChange}
-                                        />
-                                    </div>
-
-                                    <div>
-                                        <label htmlFor="contactSubject">Subject</label>
-                                        <input
-                                            type="text"
-                                            defaultValue=""
-                                            size="35"
-                                            id="contactSubject"
-                                            name="contactSubject"
-                                            onChange={this.handleChange}
-                                        />
-                                    </div>
-
-                                    <div>
-                                        <label htmlFor="contactMessage">
-                                            Message <span className="required">*</span>
-                                        </label>
-                                        <textarea
-                                            cols="50"
-                                            rows="15"
-                                            id="contactMessage"
-                                            name="contactMessage"
-                                        ></textarea>
-                                    </div>
-
-                                    <div>
-                                        <button className="submit">Submit</button>
-                                        <span id="image-loader">
-                      <img alt="" src="images/loader.gif"/>
-                    </span>
-                                    </div>
-                                </fieldset>
-                            </form>
-
-                            <div id="message-warning"> Error boy</div>
-                            <div id="message-success">
-                                <i className="fa fa-check"></i>Your message was sent, thank you!
-                                <br/>
-                            </div>
+                            <h4 className="widget-title">Videos related to my work</h4>
+                            <LiteYouTubeEmbed
+                                id="f46N8-T0Jmw"
+                                title="Video: APAC 2021: Open Source LINE FIDO2 Server Overview"
+                            />
+                            <h4 className="widget-title"> </h4>
+                            <LiteYouTubeEmbed
+                                id="JRu6IVSPEzE"
+                                title="Open source contribution Starting with LINE FIDO2 Server"
+                            />
+                            <h4 className="widget-title">YOUNGGAM-TV</h4>
+                            <LiteYouTubeEmbed
+                                id="KYPk2LJRA7A"
+                                params="&list=PLXYDdTyno5RCBoXcGz2zj0CQILoyVdIlT"
+                                title="YOUNGGAM-TV"
+                            />
                         </div>
                     </Slide>
 
