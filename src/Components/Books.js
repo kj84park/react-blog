8856fabc46fef869aaadc9f33e458434ef89f104
kyungjoon.park <kyungjoon.park@linecorp.com>
2022-03-27 22:41:07 +0900

    Add Books resources (3)

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -25,7 +25,6 @@ const Books = (props) => {
         dataType: "json",
         cache: false,
         success: function (bookList) {
-          // setPost(data);
           bookList
             .sort((a, b) => (a.id > b.id ? 1 : -1))
             .map((book) => {
@@ -54,39 +53,29 @@ const Books = (props) => {
     getBookListJson();
   }, []);
   return (
-    // <div className="App">
-    <div>
-      {/*<HeaderForBooks data={props.state.resumeData.main} />*/}
-      <div>
-        <ul>
-          {posts.map((post) => {
-            console.log("### " + post.id + " => " + post.contents);
-            return (
-              <li key={post.id}>
-                <Markdown>{post.contents}</Markdown>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      {/*<Footer data={props.state.resumeData.main} />*/}
+      <div className="App">
+        <HeaderForBooks />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="row">
+                {posts.map((post) => (
+                  <div className="col-md-4" key={post.id}>
+                    <div className="card">
+                      <div className="card-body">
+                        <h5 className="card-title">{post.fileName}</h5>
+                        <Markdown>{post.contents}</Markdown>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer data={props.state.resumeData.main} />
     </div>
   );
 };
 
 export default Books;
-
-// <div className="post-preview">
-//     <h2 className="post-title">
-//         <a href={po.link}>{po.title}</a>
-//     </h2>
-//     <h3 className="post-subtitle">
-//         <a href={po.link}>{po.subtitle}</a>
-//     </h3>
-//     <p className="post-meta">Posted by
-//         <a href={po.link}>{po.author}</a>
-//         on {po.date}</p>
-//     <Markdown>
-//         {po.content}
-//     </Markdown>
-// </div>
--- src/Components/HeaderForBooks.js
+++ src/Components/HeaderForBooks.js
@@ -1,23 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+import ParticlesBg from "particles-bg";
 
 class HeaderForBooks extends Component {
   render() {
     if (!this.props.data) return null;
-
     return (
       <header id="home">
-        {/*<ParticlesBg type="circle" bg={true} />*/}
-
         <nav id="nav-wrap">
-          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
-            Show navigation
-          </a>
-          <a className="mobile-btn" href="#home" title="Hide navigation">
-            Hide navigation
-          </a>
-
           <ul id="nav" className="nav">
             <li className="current">
               <HashLink to="/#home">HOME</HashLink>
